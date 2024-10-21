8a5049871e06cb35c2363d9a055b31081084ffbc
worldmoviezbot <emon0193861@gmail.com>
2024-10-21 19:27:17 +0600

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002427350618"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002467726173"))
 FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002328171660')).split()]
 
 # Don't Remove Credit Tg - @VJ_Botz
